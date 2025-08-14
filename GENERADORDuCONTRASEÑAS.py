078ac347943da038104b434e21861f739657db90
Santino <santinocool9@gmail.com>
2025-08-14 18:51:45 -0500

    WOWOWWOOWOWWOW
    GENERAR CONTRASEÑAS 50000000

---
+++ GENERADORDuCONTRASEÑAS.py
@@ -0,0 +1,15 @@
+import random
+
+emmmmmcosaswowow = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+
+numberofnumberscool = int(input("Please Input How many CHaracters the Password will have"))
+
+
+password = ""
+
+for i in range (numberofnumberscool):
+    JOHNPORK = random.choice(emmmmmcosaswowow)
+    password += JOHNPORK   
+print(numberofnumberscool)
+print(password)
